8c88829a95fb4aab5cd08f1c95ccdf4f37fc8d43
Gabriel Redig <glredig@gmail.com>
2015-01-10 02:46:16 -0800

    User can send feedback via contact page

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'devise'
+gem 'mail_form'
 gem 'simple_form'
 gem 'carrierwave'
 gem 'rmagick', :require => 'RMagick'
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/index"
   get "/samples", to: 'samples#index'
+  get '/contact', to: 'contacts#new'
 
   root to: 'home#index'
 
+  resources :contacts, only: [:new, :create]
   resources :events
   resources :songs
   resources :bios
