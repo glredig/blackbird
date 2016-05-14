de039e04e415240e827ea52f2af28d0ed06d56e9
Gabriel Redig <glredig@gmail.com>
2016-05-14 13:17:36 -0700

    html format back into reviews index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,7 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
 
     respond_to do |format|
+      format.html
       format.json { render json: @reviews}
     end
   end
