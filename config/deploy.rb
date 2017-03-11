e8025b35c0477b298353fab101ddbe2f87975bd2
Gabriel Redig <glredig@Gabriels-MBP.home>
2017-03-11 12:25:11 -0800

    puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,12 @@ set :repo_url, "git@github.com:glredig/blackbird.git"
 set :deploy_to, "/home/deploy/blackbird"
 set :location, "ec2-13-112-228-85.ap-northeast-1.compute.amazonaws.com"
 
+set :ssh_options, {
+  keys: %w(~/.ssh/AwsPersonal.pem),
+  forward_agent: false,
+  user: 'deploy'
+} 
+
 set :pty, true
 
 set :linked_files, %w{config/database.yml config/application.yml}
