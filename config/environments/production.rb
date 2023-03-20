cd3fe0799afa0dcae27e5283fcc85018e2a49073
Gabriel Redig <glredig@gmail.com>
2023-03-19 19:12:58 -0700

    implement mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,16 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'info@blackbirdband.com'}
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'blackbirdband.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+  # config.action_mailer.default_options = {from: 'info@blackbirdband.com'}
 
   # Default url for mail
   # config.action_mailer.default_url_options = { host: 'cloudmailin.com' }
